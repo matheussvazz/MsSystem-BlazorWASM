a4b850d74be9d82f4a11723ad40dd787ad0c3ecd
Matheus Vaz <matheusvaz1998@hotmail.com>
2023-09-29 08:53:32 -0300

    Método Post (Create)

---
+++ Configuration.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvSystem
+{
+    public class Configurations
+    {
+        public static string ApiUrl { get; set; } = "http://localhost:5095";
+    }
+}
